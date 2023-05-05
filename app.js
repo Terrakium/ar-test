f6b74440b277ebd047c3b986cba36c9117e85105
NC-DEV-Ahmet <ahmet.sahin@numericoach.fr>
2023-05-05 22:47:05 +0200

    v3

--- app.js
+++ app.js
@@ -6,22 +6,37 @@ window.onload = () => {
   el.addEventListener("gps-camera-update-position", (e) => {
     if (!testEntityAdded) {
       alert(
-        `Position déectée !: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`
+        `Position détectée !: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`
       );
       // Add a box to the north of the initial GPS position
-      const entity = document.createElement("a-box");
+      const entity = document.createElement("a-entity");
+      entity.id = "entity";
+      entity.setAttribute("gltf-model", "/assets/dragon/scene.gltf");
       entity.setAttribute("scale", {
-        x: 20,
-        y: 20,
-        z: 20,
+        x: 50,
+        y: 50,
+        z: 50,
       });
-      entity.setAttribute("material", { color: "red" });
+      entity.setAttribute("position", {
+        x: 0,
+        y: 1.5,
+        z: 0,
+      });
+      entity.setAttribute("rotation", {
+        x: 0,
+        y: 0,
+        z: 0,
+      });
+      entity.setAttribute("look-at", "[gps-new-camera]");
       entity.setAttribute("gps-new-entity-place", {
-        latitude: e.detail.position.latitude + 0.001,
+        latitude: e.detail.position.latitude + 0.0001,
         longitude: e.detail.position.longitude,
       });
+      entity.setAttribute("animation-mixer");
+      entity.setAttribute("distance");
       document.querySelector("a-scene").appendChild(entity);
     }
+
     testEntityAdded = true;
   });
 };
